f4f6d0b22a05eb17864a97b6e4f0b2d258f3c24b
Andy Russell <arussell123@gmail.com>
2015-04-24 02:27:55 -0700

    add script for generating protocol buffer code

---
+++ proto/generate.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+
+def main():
+    project_root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    java_out = os.path.join(project_root,
+                            'adb-server', 'app', 'src', 'main', 'java')
+    python_out = os.path.join(project_root,
+                              'adb-client', 'gen')
+    subprocess.call(['protoc',
+                     '--java_out={}'.format(java_out),
+                     '--python_out={}'.format(python_out),
+                     'frame.proto'])
+
+if __name__ == '__main__':
+    main()
